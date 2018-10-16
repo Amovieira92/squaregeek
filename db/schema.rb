c028910099e04116bb4a4e6d94ed143970e0776a
Sapienza e Patricia <sapienzalucas@gmail.com>
2018-10-16 19:35:36 -0300

    Solving schema conflicts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_13_182617) do
+ActiveRecord::Schema.define(version: 2018_10_15_231342) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
