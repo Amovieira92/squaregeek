03690a6c5e2ad0c4ea1a74634a54da41764beeac
HenriqueMorato <riquemorato@gmail.com>
2018-10-19 14:25:56 -0300

    wip send proposal

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'home#index'
   get 'perfil', to: 'users#profile' , as: 'my_profile'
-  resources :products only: [:new, :create, :show, :edit, :update]
+  resources :products
 end
--- spec/features/user/user_send_proposal_spec.rb
+++ spec/features/user/user_send_proposal_spec.rb
@@ -2,22 +2,12 @@ require 'rails_helper'
 
 feature 'user send proposal' do
   scenario 'successfully' do
-    hq_category = Category.create(name: 'HQs')
+    product = create(:product)
+    user = create(:user)
 
-    condition = Condition.create(name: 'Rasgado')
-
-    first_product = Product.create(
-      title: 'HQ Injustice',
-      description: 'Em capa dura',
-      category: movie_category, price: '5,00',
-      condition: condition, negotiation: :trade,
-      photo: File.new(Rails.root.join('spec', 'support', 'injustice.jpg'))
-    )
-
+    sign_in user
     visit root_path
-
-    click_on 'HQ Injustice'
-
+    click_on product.title
     click_on 'Negociar'
 
   end
